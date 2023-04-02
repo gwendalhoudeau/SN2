2c507b587661eb876b3799e313cd627c8885cba6
lhorghal <ghoudeau@gmail.com>
2023-04-02 13:55:11 +0200

    test2

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -15,10 +15,7 @@ const App = () => {
       await axios({
         method: "get",
         url: `${process.env.REACT_APP_API_URL}jwtid`,
-        withCredentials: true,
-        headers: {
-          'Access-Control-Allow-Origin':'https://sn-houdeau-gwendal.netlify.app'
-        }
+        withCredentials: true
       })
         .then((res) => {
           setUid(res.data)
